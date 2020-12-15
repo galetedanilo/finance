4ac963504f84bce5abbcbd8f105acacd7371ffb8
galetedanilo <galetedanilo@gmail.com>
2020-12-15 00:43:23 -0300

    remove constructed

--- src/gui/finance-preferences-window.c
+++ src/gui/finance-preferences-window.c
@@ -34,15 +34,6 @@ struct _FinancePreferencesWindow
 G_DEFINE_TYPE (FinancePreferencesWindow, finance_preferences_window, GTK_TYPE_DIALOG)
 
 static void
-finance_preferences_window_constructed (GObject *object)
-{
-  FinancePreferencesWindow *self = FINANCE_PREFERENCES_WINDOW (object);
-
-
-  G_OBJECT_CLASS (finance_preferences_window_parent_class)->constructed (object);
-}
-
-static void
 finance_preferences_window_dispose (GObject *object)
 {
   FinancePreferencesWindow *self = FINANCE_PREFERENCES_WINDOW (object);
@@ -57,7 +48,6 @@ finance_preferences_window_class_init (FinancePreferencesWindowClass *klass)
 {
   GtkWidgetClass  *widget_class = GTK_WIDGET_CLASS (klass);
 
-  G_OBJECT_CLASS (klass)->constructed = finance_preferences_window_constructed;
   G_OBJECT_CLASS (klass)->dispose     = finance_preferences_window_dispose;
 
   gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/Finance/gui/finance-preferences-window.ui");
