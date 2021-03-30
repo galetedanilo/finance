ed645262e6261aea87824d50e423f95c0a488841
Danilo Fernandes Galete <galetedanilo@gmail.com>
2021-03-30 20:04:38 -0300

    Set application name

--- src/main.c
+++ src/main.c
@@ -32,6 +32,8 @@ main (int   argc,
   bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
   textdomain (GETTEXT_PACKAGE);
 
+  g_set_application_name (_("Finance"));
+
   app = finance_application_new ();
 
   return g_application_run (G_APPLICATION (app), argc, argv);
