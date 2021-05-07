bb4a110c9145017c6e707a50b673c880f830b0dd
galetedanilo <galetedanilo@gmail.com>
2021-05-07 12:34:57 -0300

    Rename all methods

--- src/core/entities/finance-transaction-entity.h
+++ src/core/entities/finance-transaction-entity.h
@@ -1,4 +1,4 @@
-/* finance-object-transaction.h
+/* finance-transaction-entity.h
  *
  * Copyright 2021 Danilo Fernandes Galete <galetedanilo@gmail.com>
  *
@@ -18,94 +18,99 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-#ifndef __FINANCE_OBJECT_TRANSACTION_H__
-#define __FINANCE_OBJECT_TRANSACTION_H__
+#ifndef __FINANCE_TRANSACTION_ENTITY_H__
+#define __FINANCE_TRANSACTION_ENTITY_H__
 
 #include <glib-object.h>
 
 G_BEGIN_DECLS
 
-#define FINANCE_TYPE_OBJECT_TRANSACTION (finance_object_transaction_get_type())
+#define FINANCE_TYPE_TRANSACTION_ENTITY (finance_transaction_entity_get_type())
 
-G_DECLARE_FINAL_TYPE (FinanceObjectTransaction, finance_object_transaction, FINANCE, OBJECT_TRANSACTION, GObject)
+G_DECLARE_FINAL_TYPE (FinanceTransactionEntity, finance_transaction_entity, FINANCE, TRANSACTION_ENTITY, GObject)
 
-FinanceObjectTransaction *      finance_object_transaction_new                          (void);
+FinanceTransactionEntity *      finance_transaction_entity_new                          (void);
 
-gdouble                         finance_object_transaction_get_amount                   (FinanceObjectTransaction *self);
+gdouble                         finance_transaction_entity_get_amount                   (FinanceTransactionEntity *self);
 
-void                            finance_object_transaction_set_amount                   (FinanceObjectTransaction *self,
+void                            finance_transaction_entity_set_amount                   (FinanceTransactionEntity *self,
                                                                                          gdouble                   amount);
 
-const gchar *                   finance_object_transaction_get_category                 (FinanceObjectTransaction *self);
+const gchar *                   finance_transaction_entity_get_category                 (FinanceTransactionEntity *self);
 
-void                            finance_object_transaction_set_category                 (FinanceObjectTransaction *self,
+void                            finance_transaction_entity_set_category                 (FinanceTransactionEntity *self,
                                                                                          const gchar              *category);
 
-GdkRGBA *                       finance_object_transaction_get_color                    (FinanceObjectTransaction *self);
+GDateTime *                     finance_transaction_entity_get_date                     (FinanceTransactionEntity *self);
 
-void                            finance_object_transaction_set_color                    (FinanceObjectTransaction *self,
-                                                                                         const GdkRGBA            *color);
-
-GDateTime *                     finance_object_transaction_get_date                     (FinanceObjectTransaction *self);
-
-void                            finance_object_transaction_set_date                     (FinanceObjectTransaction *self,
+void                            finance_transaction_entity_set_date                     (FinanceTransactionEntity *self,
                                                                                          GDateTime                *date);
 
-gint                            finance_object_transaction_get_frequency                (FinanceObjectTransaction *self);
+gint                            finance_transaction_entity_get_frequency                (FinanceTransactionEntity *self);
 
-void                            finance_object_transaction_set_frequency                (FinanceObjectTransaction *self,
+void                            finance_transaction_entity_set_frequency                (FinanceTransactionEntity *self,
                                                                                          gint                      frequency);
 
-GDateTime *                     finance_object_transaction_get_frequency_date           (FinanceObjectTransaction *self);
+GDateTime *                     finance_transaction_entity_get_frequency_date           (FinanceTransactionEntity *self);
 
-void                            finance_object_transaction_set_frequency_date           (FinanceObjectTransaction *self,
+void                            finance_transaction_entity_set_frequency_date           (FinanceTransactionEntity *self,
                                                                                          GDateTime                *date);
 
-gint                            finance_object_transaction_get_frequency_number         (FinanceObjectTransaction *self);
+gint                            finance_transaction_entity_get_frequency_number         (FinanceTransactionEntity *self);
 
-void                            finance_object_transaction_set_frequency_number         (FinanceObjectTransaction *self,
+void                            finance_transaction_entity_set_frequency_number         (FinanceTransactionEntity *self,
                                                                                          gint                      frequency_number);
 
-const gchar *                   finance_object_transaction_get_icon                     (FinanceObjectTransaction *self);
+GdkRGBA *                       finance_transaction_entity_get_icon_color               (FinanceTransactionEntity *self);
 
-void                            finance_object_transaction_set_icon                     (FinanceObjectTransaction *self,
-                                                                                         const gchar              *icon);
+void                            finance_transaction_entity_set_icon_color               (FinanceTransactionEntity *self,
+                                                                                         const GdkRGBA            *color);
 
-const gchar *                   finance_object_transaction_get_name                     (FinanceObjectTransaction *self);
+const gchar *                   finance_transaction_entity_get_icon_name                (FinanceTransactionEntity *self);
 
-void                            finance_object_transaction_set_name                     (FinanceObjectTransaction *self,
+void                            finance_transaction_entity_set_icon_name                (FinanceTransactionEntity *self,
                                                                                          const gchar              *name);
 
-const gchar *                   finance_object_transaction_get_notes                    (FinanceObjectTransaction *self);
+const gchar *                   finance_transaction_entity_get_icon_text                (FinanceTransactionEntity *self);
 
-void                            finance_object_transaction_set_notes                    (FinanceObjectTransaction *self,
+void                            finance_transaction_entity_set_icon_text                (FinanceTransactionEntity *self,
+                                                                                         const gchar              *text);
+
+const gchar *                   finance_transaction_entity_get_name                     (FinanceTransactionEntity *self);
+
+void                            finance_transaction_entity_set_name                     (FinanceTransactionEntity *self,
+                                                                                         const gchar              *name);
+
+const gchar *                   finance_transaction_entity_get_notes                    (FinanceTransactionEntity *self);
+
+void                            finance_transaction_entity_set_notes                    (FinanceTransactionEntity *self,
                                                                                          const gchar              *notes);
 
-const gchar *                   finance_object_transaction_get_payee_name               (FinanceObjectTransaction *self);
+const gchar *                   finance_transaction_entity_get_payee_name               (FinanceTransactionEntity *self);
 
-void                            finance_object_transaction_set_payee_name               (FinanceObjectTransaction *self,
+void                            finance_transaction_entity_set_payee_name               (FinanceTransactionEntity *self,
                                                                                          const gchar              *payee_name);
 
-gint                            finance_object_transaction_get_payment                  (FinanceObjectTransaction *self);
+gint                            finance_transaction_entity_get_payment                  (FinanceTransactionEntity *self);
 
-void                            finance_object_transaction_set_payment                  (FinanceObjectTransaction *self,
+void                            finance_transaction_entity_set_payment                  (FinanceTransactionEntity *self,
                                                                                          gint                      payment);
 
-const gchar *                   finance_object_transaction_get_payment_information      (FinanceObjectTransaction *self);
+const gchar *                   finance_transaction_entity_get_payment_information      (FinanceTransactionEntity *self);
 
-void                            finance_object_transaction_set_payment_information      (FinanceObjectTransaction *self,
+void                            finance_transaction_entity_set_payment_information      (FinanceTransactionEntity *self,
                                                                                          const gchar              *information);
 
-gint                            finance_object_transaction_get_repeat                   (FinanceObjectTransaction *self);
+gint                            finance_transaction_entity_get_repeat                   (FinanceTransactionEntity *self);
 
-void                            finance_object_transaction_set_repeat                   (FinanceObjectTransaction *self,
+void                            finance_transaction_entity_set_repeat                   (FinanceTransactionEntity *self,
                                                                                          gint                      repeat);
 
-gint                            finance_object_transaction_get_type                     (FinanceObjectTransaction *self);
+gint                            finance_transaction_entity_get_type                     (FinanceTransactionEntity *self);
 
-void                            finance_object_transaction_set_type                     (FinanceObjectTransaction *self,
+void                            finance_transaction_entity_set_type                     (FinanceTransactionEntity *self,
                                                                                          gint                      type);
 
 G_END_DECLS
 
-#endif /* __FINANCE_OBJECT_TRANSACTION_H__ */
+#endif /* __FINANCE_TRANSACTION_ENTITY_H__ */
